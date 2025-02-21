0b8ada5228b73eb0265d4433d6ac19ac3069eaaf
Gahel <music@gahel.fr>
2025-02-21 20:39:56 +0100

    Fix: run all ancestors .env when entering folder, run all non common ancestors .env.leave when changing

--- src/main.rs
+++ src/main.rs
@@ -45,19 +45,27 @@ fn main() {
 
     let mut output = Output::new();
 
-
     // only run .env.leave if getting out of directory, not deeper inside
     if !target.starts_with(&source) {
-        if let Some(f) = get_envleave_file(&source) {
-            output.push(shell_command::source(f.to_str().unwrap()));
+        // find all .env.leave files of source ancestor folders that are not ancestors of target
+        for ancestor in source.ancestors() {
+            if !target.starts_with(ancestor) {
+                if let Some(f) = get_envleave_file(ancestor) {
+                    output.push(shell_command::source(f.to_str().unwrap()));
+                }
+            }
         }
     }
 
-    output.push(shell_command::cd(target.to_str().unwrap()));
-
-    if let Some(f) = get_env_file(&target) {
-        output.push(shell_command::source(f.to_str().unwrap()));
+    // run all parent folders env files
+    for ancestor in target.ancestors() {
+        if let Some(f) = get_env_file(ancestor) {
+            output.push(shell_command::cd(ancestor.to_str().unwrap()));
+            output.push(shell_command::source(f.to_str().unwrap()));
+        };
     }
+
+    output.push(shell_command::cd(target.to_str().unwrap()));
 
     output.send()
 }
